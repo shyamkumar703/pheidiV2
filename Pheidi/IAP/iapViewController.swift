3770aa45b0f6fe929cd59d46243fb6718f83473f
Shyam Kumar <shyamkumar@berkeley.edu>
2020-11-07 01:00:15 -0800

    New IAP option

--- Pheidi/IAP/iapViewController.swift
+++ Pheidi/IAP/iapViewController.swift
@@ -13,22 +13,70 @@ import StatusAlert
 class iapViewController: UIViewController {
     @IBOutlet weak var joinButton: UIButton!
     @IBOutlet weak var restoreButton: UIButton!
+    @IBOutlet weak var popularLabel: UILabel!
+    @IBOutlet weak var yearSubscription: UIView!
+    @IBOutlet weak var monthSubscription: UIView!
+    
+    var monthSelected = false
+    var selectedProductIdentifier = "com.pheidi.pheidiProYear"
     
     override func viewDidLoad() {
         super.viewDidLoad()
 //        let alphaColor = UIColor(red:0.00, green:0.70, blue:0.56, alpha:1.0)
 //        pheidiColors.addGradientColorToView(view: self.view, color: alphaColor)
         
-        joinButton.layer.cornerRadius = 15
-        joinButton.layer.borderColor = pheidiColors.pheidiTeal.cgColor
-        joinButton.layer.borderWidth = 1
-        
+        joinButton.layer.cornerRadius = 10
+        joinButton.backgroundColor = pheidiColors.pheidiTeal
 //        restoreButton.layer.cornerRadius = 10
 //        restoreButton.layer.borderColor = UIColor.black.cgColor
 //        restoreButton.layer.borderWidth = 1
         
+        popularLabel.layer.cornerRadius = 3
+        popularLabel.layer.masksToBounds = true
+        
+        yearSubscription.layer.cornerRadius = 10
+        yearSubscription.layer.borderColor = pheidiColors.pheidiTeal.cgColor
+        yearSubscription.layer.borderWidth = 1.0
+        let yearTapGesture = UITapGestureRecognizer(target: self, action: #selector(self.yearTapped))
+        yearSubscription.addGestureRecognizer(yearTapGesture)
+        
+        monthSubscription.layer.cornerRadius = 10
+        let monthTapGesture = UITapGestureRecognizer(target: self, action: #selector(self.monthTapped))
+        monthSubscription.addGestureRecognizer(monthTapGesture)
+        
+        
+        
+        
+        
         SKPaymentQueue.default().add(self)
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func yearTapped(_sender: UITapGestureRecognizer) {
+        selectedProductIdentifier = "com.pheidi.pheidiProYear"
+        if monthSelected {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.monthSubscription.layer.borderColor = UIColor.black.cgColor
+                self.popularLabel.backgroundColor = pheidiColors.pheidiTeal
+                self.yearSubscription.layer.borderColor = pheidiColors.pheidiTeal.cgColor
+                self.joinButton.setTitle("Get 12 Months For $23.88", for: .normal)
+                self.monthSelected = false
+            })
+        }
+    }
+    
+    @objc func monthTapped(_sender: UITapGestureRecognizer) {
+        selectedProductIdentifier = "com.Pheidi.pheidiPro"
+        if !monthSelected {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.yearSubscription.layer.borderColor = UIColor.black.cgColor
+                self.popularLabel.backgroundColor = UIColor(red:0.59, green:0.59, blue:0.59, alpha:1.0)
+                self.monthSubscription.layer.borderColor = pheidiColors.pheidiTeal.cgColor
+                self.monthSubscription.layer.borderWidth = 1.0
+                self.joinButton.setTitle("Get One Month For $3.00", for: .normal)
+                self.monthSelected = true
+            })
+        }
     }
     
     @IBAction func close(_ sender: Any) {
@@ -77,6 +125,7 @@ extension iapViewController: SKPaymentTransactionObserver {
                 SKStoreReviewController.requestReview()
                 
             } else if transaction.transactionState == .failed {
+                
                 let generator = UINotificationFeedbackGenerator()
                 generator.notificationOccurred(.error)
                 
@@ -110,7 +159,7 @@ extension iapViewController: SKPaymentTransactionObserver {
     func paymentRequested() {
         if SKPaymentQueue.canMakePayments() {
             let paymentRequest = SKMutablePayment()
-            paymentRequest.productIdentifier = "com.Pheidi.pheidiPro"
+            paymentRequest.productIdentifier = selectedProductIdentifier
             SKPaymentQueue.default().add(paymentRequest)
         } else {
             print("User unable to make payments")
